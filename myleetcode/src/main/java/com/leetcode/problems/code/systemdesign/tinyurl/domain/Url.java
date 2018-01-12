3db948287c1f0577ca70ae9e8f40aa6493cc90d2
yanlilong <yan.w.li@outlook.com>
2018-01-12 14:07:35 +0100

    create url class

--- myleetcode/src/main/java/com/leetcode/problems/code/systemdesign/tinyurl/domain/Url.java
+++ myleetcode/src/main/java/com/leetcode/problems/code/systemdesign/tinyurl/domain/Url.java
@@ -3,13 +3,33 @@ package com.leetcode.problems.code.systemdesign.tinyurl.domain;
 import java.io.Serializable;
 
 public class Url implements Identifiable {
-  private String 
+
+  private Long id;
+  private String longURL;
+  private String shortURL;
+
+  public String getLongURL() {
+    return longURL;
+  }
+
+
+  public String getShortURL() {
+    return shortURL;
+  }
+
+  public void setShortURL(String shortURL) {
+    this.shortURL = shortURL;
+  }
+
+  public void setLongURL(String longURL) {
+    this.longURL = longURL;
+  }
 
   public void setId(Long id) {
-
+    id = id;
   }
 
   public Serializable getId() {
-    return null;
+    return id;
   }
 }
