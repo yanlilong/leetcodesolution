74fe29f730af566a0a298d3b1054338c35af2681
yanlilong <yan.w.li@outlook.com>
2018-01-17 23:31:19 +0100

    add URL Repository interface

---
+++ myleetcode/src/main/java/com/leetcode/problems/code/systemdesign/tinyurl/repository/URLRepository.java
@@ -0,0 +1,10 @@
+package com.leetcode.problems.code.systemdesign.tinyurl.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
+import com.leetcode.problems.code.systemdesign.tinyurl.domain.Url;
+@Transactional
+public interface URLRepository extends CrudRepository<Url, Long> {
+
+
+}
