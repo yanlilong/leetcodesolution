5bc13caf82694b63a69485107565ecf1f9d27d12
yanlilong <yan.w.li@outlook.com>
2018-01-17 23:29:01 +0100

    change type of id

--- myleetcode/src/main/java/com/leetcode/problems/code/systemdesign/tinyurl/domain/Identifiable.java
+++ myleetcode/src/main/java/com/leetcode/problems/code/systemdesign/tinyurl/domain/Identifiable.java
@@ -3,7 +3,7 @@ package com.leetcode.problems.code.systemdesign.tinyurl.domain;
 
 import java.io.Serializable;
 
-public interface Identifiable extends org.springframework.hateoas.Identifiable<Long> {
+/**public interface Identifiable extends org.springframework.hateoas.Identifiable<Long> {
 
   public void setId(Long id);
-}
+}*/
--- myleetcode/src/main/java/com/leetcode/problems/code/systemdesign/tinyurl/domain/Url.java
+++ myleetcode/src/main/java/com/leetcode/problems/code/systemdesign/tinyurl/domain/Url.java
@@ -1,19 +1,63 @@
 package com.leetcode.problems.code.systemdesign.tinyurl.domain;
 
 import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-public class Url implements Identifiable {
+/**
+ * public class Url implements Identifiable {
+ *
+ * private Long id; private String longURL; private String shortURL;
+ *
+ * public String getLongURL() { return longURL; }
+ *
+ *
+ *
+ *
+ * public String getShortURL() { return shortURL; }
+ *
+ * public void setShortURL(String shortURL) { this.shortURL = shortURL; }
+ *
+ * public void setLongURL(String longURL) { this.longURL = longURL; }
+ *
+ * public void setId(Long id) { id = id; }
+ *
+ * public Long getId() { return id; }
+ *
+ * }
+ */
+@Entity
+@Table(name = "url")
 
-  private Long id;
+public class Url {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+
+private Integer id;
   private String longURL;
   private String shortURL;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
   public String getLongURL() {
     return longURL;
   }
 
-
-
+  public void setLongURL(String longURL) {
+    this.longURL = longURL;
+  }
 
   public String getShortURL() {
     return shortURL;
@@ -21,17 +65,5 @@ public class Url implements Identifiable {
 
   public void setShortURL(String shortURL) {
     this.shortURL = shortURL;
-  }
-
-  public void setLongURL(String longURL) {
-    this.longURL = longURL;
-  }
-
-  public void setId(Long id) {
-    id = id;
-  }
-
-  public Long getId() {
-    return id;
   }
 }
